e587a4859eda57612568a2f17b2670b9c4d14339
seki-eiji <seki-eiji@KYCNT1901-10.local>
2019-03-04 15:41:22 +0900

    Add: arrayRefV3.pl: 配列リファレンスの要素取り出し

---
+++ chapter03/arrayRefV3.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/env perl -w
+
+my @month = ('January', 'February', 'March', 'April', 'May', 'June',
+             'July', 'August', 'September', 'October', 'Novenmber', 'December');
+
+my $refMonth = \@month;
+
+print "refMonth: $refMonth -> @$refMonth No.5: $$refMonth[4] \n";
