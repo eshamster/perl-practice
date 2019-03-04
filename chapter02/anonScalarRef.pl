cbae79329e13bab7a0d962d9648c92ea0fb0e7a0
seki-eiji <seki-eiji@KYCNT1901-10.local>
2019-03-04 14:25:46 +0900

    Add: anonScalarRef.pl

---
+++ chapter02/anonScalarRef.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/env perl -w
+
+$refAge = \14;
+
+print "refAge is $refAge and it contains $$refAge \n";
+
+$refName = \"Ikaruga";
+
+print "refName is $refName and it contains $$refName \n";
