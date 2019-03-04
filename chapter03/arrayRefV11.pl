9e8f4898e194fe70f650ccb34abff9f8447ca3fa
seki-eiji <seki-eiji@KYCNT1901-10.local>
2019-03-04 17:00:26 +0900

    Add: arrayRefV11.pl: 無名配列を使った2次元配列の初期化

---
+++ chapter03/arrayRefV11.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/env perl -w
+
+my @month = (
+    ['January',   31],
+    ['February',  28],
+    ['March',     31],
+    ['April',     30],
+    ['May',       31],
+    ['June',      30],
+    ['July',      31],
+    ['August',    31],
+    ['September', 30],
+    ['October',   31],
+    ['Novenmber', 30],
+    ['December',  31],
+    );
+    
+print "the 4th month is $month[3][0] and it has $month[3][1] days\n"
