6c0fb19ea2119cef8997d3d5964f6f54dabc4555
eshamster <hamgoostar@gmail.com>
2019-03-05 17:01:22 +0900

    Update: Note

--- chapter07/myExp.pl
+++ chapter07/myExp.pl
@@ -5,6 +5,8 @@ my $i = 'robot';
 # Note:
 # 下記のmyを取っても最後の出力は"I, robot."になる。
 # forの変数は暗黙にlexicalになる？(そうした仕様は見つけられず)
+# → myなしでstrict 'vars'をつけると怒られるのでlexicalにはなっていないはず。
+#   挙動的には抜けるときに元の値に書き戻されているような感じがする
 for my $i (1..5) {
     &kurikaeshiShori($i);
 }
