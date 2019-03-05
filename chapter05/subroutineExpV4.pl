6d22f375f00b1a949c04e44ebc10b6e7f00e13b3
seki-eiji <seki-eiji@KYCNT1901-10.local>
2019-03-05 10:49:28 +0900

    Add: subroutineExpV4.pl: ->

---
+++ chapter05/subroutineExpV4.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/env perl -w
+
+my $codeRef = \&daikeiNoMenseki;
+my $menseki = $codeRef->(3, 4, 5);
+print "menseki wa $menseki desu! codeRef wa $codeRef desu! \n";
+
+sub daikeiNoMenseki {
+    my ($joutei, $katei, $takasa) = @_;
+    warn "korekara daikei no menseki wo keisan simasu !!! ".
+        "joutei: $joutei, katei: $katei, takasa: $takasa \n";
+    my $menseki = ($joutei + $katei) * $takasa / 2;
+    return $menseki
+}
