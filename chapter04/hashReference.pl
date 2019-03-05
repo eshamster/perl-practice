a6b543d900edf5c33d4a0dc018b01554136cb2a5
seki-eiji <seki-eiji@KYCNT1901-10.local>
2019-03-05 09:27:47 +0900

    Add: hashReference.pl: ハッシュ変数のリファレンス

---
+++ chapter04/hashReference.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/env perl -w
+
+my %month = (
+    January    => 1,
+    February   => 2,
+    March      => 3,
+    April      => 4,
+    May        => 5,
+    June       => 6,
+    July       => 7,
+    August     => 8,
+    September  => 9,
+    October    => 10,
+    Novenmber  => 11,
+    December   => 12,
+    );
+
+my $refMonth = \%month;
+print "ref to month hash: $refMonth \n";
+
+my @sortedKey = sort keys %$refMonth;
+print "sorted key: @sortedKey \n";
+
+print "January is Month No. $$refMonth{January} \n";
+print "February is Month No. $refMonth->{February} \n";
