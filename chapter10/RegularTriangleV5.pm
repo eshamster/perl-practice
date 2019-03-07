703f6e457adad227188f352ca9b4f3d30f21e336
eshamster <hamgoostar@gmail.com>
2019-03-07 09:48:01 +0900

    Update: RegularTriangleV5.pm: いっそ継承をやめてuseだけする版

--- chapter10/RegularTriangleV5.pm
+++ chapter10/RegularTriangleV5.pm
@@ -3,22 +3,20 @@ package RegularTriangle;
 use strict;
 
 use TriangleV4;
-our @ISA = qw(Triangle);
 
 sub space {
     my ($class, $a) = @_;
-    Triangle->space($a, $a, $a);
+    Triangle::space($class, $a, $a, $a);
 }
 
 1;
 
 =pod
 実行結果：
-※2つ目の "You are ~"を"Triangle"でなく"RegularTriangle"にしたい
 
 $ ./triangleSpaceV5.pl 
 You are able to calculate the space of Triangle !!! 
 6
-You are able to calculate the space of Triangle !!! 
+You are able to calculate the space of RegularTriangle !!! 
 15.5884572681199
 =cut
