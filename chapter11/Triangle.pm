4dfa9fc7bc30fbb5b64fa12cb184474386ba0bd6
eshamster <hamgoostar@gmail.com>
2019-03-07 11:58:37 +0900

    Refactor: Triangle.pm: 少し短くする

--- chapter11/Triangle.pm
+++ chapter11/Triangle.pm
@@ -4,13 +4,11 @@ use strict;
 
 sub new {
     my ($class, $a, $b, $c) = @_;
-    my $self = { a => $a, b => $b, c => $c };
-    bless $self, $class;
-    return $self;
+    bless { a => $a, b => $b, c => $c };
 }
 
 sub space {
-    my ($self) = @_;
+    my $self = shift;
     warn "You are about to calculate the space of ", ref($self), " !!! \n";
     my ($a, $b, $c) = ($self->{a}, $self->{b}, $self->{c});
     my $s = ($a + $b + $c) / 2;
