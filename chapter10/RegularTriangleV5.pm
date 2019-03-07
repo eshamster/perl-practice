5f32eb9ac6219c7d0abc67bbc1e1b6353ac2c086
eshamster <hamgoostar@gmail.com>
2019-03-07 09:56:46 +0900

    Update: RegularTriangleV5.pm: 継承なしで、存在しないTriangle::spaceを直接呼び出した版

--- chapter10/RegularTriangleV5.pm
+++ chapter10/RegularTriangleV5.pm
@@ -3,24 +3,22 @@ package RegularTriangle;
 use strict;
 
 use TriangleV4;
-our @ISA = qw(Triangle);
 
 sub space {
     my ($class, $a) = @_;
-    Triangle->space($a, $a, $a);
+    Triangle::space($class, $a, $a, $a);
 }
 
 1;
 
 =pod
 実行結果：
-※Triangleがspaceの実装を放棄したので、Figure::spaceが実行されている
+※存在しないTriangle::spaceを直接呼ぼうとしたため、
+  親のFigure::spaceまで辿り着けていない
 
 $ ./triangleSpaceV5.pl 
 you  are about to calculate the space of Triangle using the argment 3, 4, 5 !!! 
 But I don't know how to do it! 
 UNKNOWN
-you  are about to calculate the space of Triangle using the argment 6, 6, 6 !!! 
-But I don't know how to do it! 
-UNKNOWN
+Undefined subroutine &Triangle::space called at RegularTriangleV5.pm line 9.
 =cut
